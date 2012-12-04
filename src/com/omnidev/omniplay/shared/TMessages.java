5f3fbbc49d13edc9b79a7fc8d268f1f4c5ade418
iccthedral <nullpotent@gmail.com>
2012-12-04 22:52:54 +0100

    removed files

--- src/com/omnidev/omniplay/shared/TMessages.java
+++
@@ -1,8 +0,0 @@
-package com.omnidev.omniplay.shared;
-
-public class TMessages {
-	public static final String		  	MP3_HEADER			= "MP3_HEADER";
-	public static final String 	  	  	MP3_FOOTER   		= "MP3_FOOTER";
-	public static final String 			MP3_MESSAGE			= "MP3_MESSAGE";
-	public static final String			STRING_MESSAGE		= "STRING_MESSAGE";
-}
